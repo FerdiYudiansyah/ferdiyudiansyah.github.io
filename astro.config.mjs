d818f1ce0a4c137462562452c49a3d9ec7b51bea
Ferdi <ferdiyudiansyahme@gmail.com>
2026-09-08 17:56:42 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ferdiyudiansyah.github.io',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
 });
